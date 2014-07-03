8ddfc0fea95304fedd1b26fdbc0b4229659aa881
Matt Savage <matthew.savage@gmail.com>
2014-07-04 00:34:06 +0100

    Starting 1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject savagematt/bowen "1.0"
+(defproject savagematt/bowen "1.1-SNAPSHOT"
   :description "Implements the decorator pattern for clojure protocols."
 
   :url "http://github.com/savagematt/bowen"
